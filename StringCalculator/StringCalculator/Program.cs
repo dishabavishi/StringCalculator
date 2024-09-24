a972d64d6d424fb08d02e9924115d76509c7dd1f
dishabavishi <dishabavishi24@gmail.com>
2024-09-25 01:07:16 +0530

    Throw exception if any negative number is found in input

--- StringCalculator/StringCalculator/Program.cs
+++ StringCalculator/StringCalculator/Program.cs
@@ -2,7 +2,7 @@
 {
     public static void Main(string[] args)
     {
-        string input = "//;\n1;2;3;5";
+        string input = "//;\n1;-2;6;-8";
         var delimeterAndInput = input.Split("\n");
         string delimeter = delimeterAndInput[0]?.Length > 0 ? delimeterAndInput[0].Substring(delimeterAndInput[0].Length - 1) : ",";
         var output = Add(delimeterAndInput[1], delimeter);
@@ -13,6 +13,11 @@
     {
         var nums = numbers.Split(delimeter)
             .Select(n => !string.IsNullOrEmpty(n) ? int.Parse(n) : 0);
+        var negativeNumbers = nums.Where(x => x < 0);
+        if (negativeNumbers.Any())
+        {
+            throw new Exception($"negative numbers not allowed {string.Join(",", negativeNumbers)}");
+        }
         return nums.Sum(n => n);
     }
 }

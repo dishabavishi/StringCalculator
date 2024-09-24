d477606f86c83b01ad9dc3486af3c3faf771cd4d
dishabavishi <dishabavishi24@gmail.com>
2024-09-25 00:23:51 +0530

    Add Main and add method with basic case

--- StringCalculator/StringCalculator/Program.cs
+++ StringCalculator/StringCalculator/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string input = "";
+        var output = add(input);
+        Console.WriteLine(output);
+    }
+
+    public static int add(string numbers)
+    {
+        var nums = numbers.Split(',').Select(x => !string.IsNullOrEmpty(x) ? int.Parse(x) : 0);
+        return nums.Sum(n => n);
+    }
+}
